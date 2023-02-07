37f045a82414a8dbfa200bdb66c964077ba2360f
Kanav Dahat <kanavdahat55@gmail.com>
2023-02-07 17:04:39 +0530

    react-router-dom added

--- src/components/News.js
+++ src/components/News.js
@@ -23,43 +23,31 @@ export class News extends Component {
             page:1
           }
         }
-        async componentDidMount(){
+
+        async updateNews(){
           let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c562d548a8bd4716946408283b80acf3&page=${this.state.page}&pageSize=${this.props.pageSize}`
           this.setState({loading:true});
           let data=await fetch(url);
           let parsedData=await data.json();
-          console.log(parsedData);
+          //console.log(parsedData);
           this.setState({
             articles:parsedData.articles,
             totalResults:parsedData.totalResults,
             loading:false
           })
         }
+        async componentDidMount(){
+          this.updateNews();
+        }
 
         handlePrevClick=async()=>{
-          let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c562d548a8bd4716946408283b80acf3&page=${this.state.page-1}&pageSize=${this.props.pageSize}`
-          this.setState({loading:true});
-          let data=await fetch(url);
-          let parsedData=await data.json();
-          //console.log(parsedData);
-          this.setState({
-            page:this.state.page-1,
-            articles:parsedData.articles,
-            loading:false
-          })
+          this.setState({page:this.state.page-1})
+          this.updateNews();
         }
 
         handleNextClick=async()=>{
-          let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c562d548a8bd4716946408283b80acf3&page=${this.state.page+1}&pageSize=${this.props.pageSize}`
-          this.setState({loading:true});
-          let data=await fetch(url);
-          let parsedData=await data.json();
-          //console.log(parsedData);
-          this.setState({
-            page:this.state.page+1,
-            articles:parsedData.articles,
-            loading:false
-          })
+          this.setState({page:this.state.page+1})
+          this.updateNews();
         }
   render() {
     return (
@@ -74,7 +62,7 @@ export class News extends Component {
           {!this.state.loading && this.state.articles.map((element)=>{
             return <div key={element.url}className='col-md-4 '>
                        <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} 
-                      imageUrl={element.urlToImage} newsUrl={element.url}/>
+                      imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
                    </div>
           })} 
         </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title,description,imageUrl,newsUrl}=this.props;
+    let {title,description,imageUrl,newsUrl,author,date,source}=this.props;
     return (
       <div className='my-3'>
         <div className="card">
@@ -10,7 +10,9 @@ export class NewsItem extends Component {
   <div className="card-body">
     <h5 className="card-title">{title}...</h5>
     <p className="card-text">{description}...</p>
-    <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read More</a>
+    <span className="badge text-bg-info">Source-{source}</span>
+    <p className="card-text"><small className="text-muted">By {author?author:"Unknown"} on {new Date(date).toUTCString()}</small></p>
+    <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read More</a>
   </div>
 </div>
       </div>
